c3c3da1f3c309411b58fa4bd5141b884e4bfb56f
barbac <jjbarbac+git@gmail.com>
2014-07-08 01:37:31 -0500

    Add properties to CAPAlert

--- capalert.h
+++ capalert.h
@@ -30,8 +30,42 @@ enum class Scope {
 
 class CAPAlert: public QObject
 {
+    Q_OBJECT
+
+    Q_PROPERTY(QString identifier NOTIFY identifierChanged MEMBER m_identifier)
+    Q_PROPERTY(QString sender NOTIFY senderChanged MEMBER m_sender)
+    Q_PROPERTY(QString sent NOTIFY sentChanged MEMBER m_sent)
+    Q_PROPERTY(Status status NOTIFY statusChanged MEMBER m_status)
+    Q_PROPERTY(MsgType msgType NOTIFY msgTypeChanged MEMBER m_msgType)
+    //Q_PROPERTY(QString source NOTIFY sourceChanged MEMBER m_source)
+    Q_PROPERTY(Scope scope NOTIFY scopeChanged MEMBER m_scope)
+    //Q_PROPERTY(QString restriction NOTIFY restrictionChanged MEMBER m_restriction)
+    //Q_PROPERTY(QString addresses NOTIFY addressesChanged MEMBER m_addresses)
+    Q_PROPERTY(QString code NOTIFY codeChanged MEMBER m_code)
+    //Q_PROPERTY(QString note NOTIFY noteChanged MEMBER m_note)
+    //Q_PROPERTY(QString references NOTIFY referencesChanged MEMBER m_references)
+    //Q_PROPERTY(QString incidents NOTIFY incidentsChanged MEMBER m_incidents)
+    Q_PROPERTY(CAPInfo * info NOTIFY infoChanged MEMBER m_info)
+
+signals:
+    void identifierChanged();
+    void senderChanged();
+    void sentChanged();
+    void statusChanged();
+    void msgTypeChanged();
+    //void sourceChanged();
+    void scopeChanged();
+    //void restrictionChanged();
+    //void addressesChanged();
+    void codeChanged();
+    //void noteChanged();
+    //void referencesChanged();
+    //void incidentsChanged();
+    void infoChanged();
+
 public:
     explicit CAPAlert(QObject *parent);
+
     QString m_identifier;
     QString m_sender;
     QString m_sent;
